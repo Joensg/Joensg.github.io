09e01e3440a28e8908d4dcdff2c93e72a15e6de0
Joensg <joen.sg@gmail.com>
2015-11-18 15:21:22 +0000

    revision 5
    added contact page

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+var myCenter=new google.maps.LatLng(51.582713, -0.340729);
+
+function initialize() {
+    var mapProp = {center:myCenter, zoom:10, mapTypeId:google.maps.MapTypeId.ROADMAP};
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var marker=new google.maps.Marker({position:myCenter,});
+    marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
